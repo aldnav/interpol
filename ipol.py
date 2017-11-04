4da66a32e20a2f25a08ffde22c4697aeca875160
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-04 22:39:23 +0800

    Refactor error display

--- ipol.py
+++ ipol.py
@@ -725,7 +725,9 @@ if __name__ == '__main__':
                     postfix_evaluator.evaluate()
                     evaluation_error = postfix_evaluator.error_msg
                     if evaluation_error:
-                        print '%s: %s %s' % (filename, i + 1, evaluation_error)
+                        print '%s\n%s %s' % (evaluation_error,
+                            filename, i + 1)
+                        print '  ->', line
                         has_error = True
                         break
                     parse_list = []
